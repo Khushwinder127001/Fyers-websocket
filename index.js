efa6cae09abd84e0b65d6fc917e10ea2d384b32e
Khushwinder127001 <khushwinder.php@gmail.com>
2023-09-18 10:34:18 +0530

    Third Commit

--- index.js
+++ index.js
@@ -13,16 +13,17 @@ app.listen(port, () => {
 
 const FyersSocket = require("fyers-api-v3").fyersDataSocket
 
-var fyersdata= new FyersSocket("Y4PJ649WNP-100:eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJhcGkuZnllcnMuaW4iLCJpYXQiOjE2OTQ5NDg4NzksImV4cCI6MTY5NDk5NzA1OSwibmJmIjoxNjk0OTQ4ODc5LCJhdWQiOlsieDowIiwieDoxIiwieDoyIiwiZDoxIiwiZDoyIiwieDoxIiwieDowIl0sInN1YiI6ImFjY2Vzc190b2tlbiIsImF0X2hhc2giOiJnQUFBQUFCbEJ0NFBVekJic2EzWnBZdWEtR2xSYmlaR3JDdmFROTRMSmxjMjdEQTB5b2lOTEZjZUpmLWZ4QkVmTEs0dGxpYTdhaTJKNDFzWEtqUVM1MGNoVGtmS3VlbUF5c1l1QVFqdjBfeGJYTEFIWGZEV1AxRT0iLCJkaXNwbGF5X25hbWUiOiJLSFVTSFdJTkRFUiIsIm9tcyI6IksxIiwiaHNtX2tleSI6IjY2ZTBiYzM3MmQ4ZWVkMmNkNDdhNmJjNmJmYjY4MzY4MWRlN2Q5ZTg1MGU2YTlhYWI5NjgzMTkyIiwiZnlfaWQiOiJYSzAzNDgxIiwiYXBwVHlwZSI6MTAwLCJwb2FfZmxhZyI6Ik4ifQ.h_qe0a-p_kDdjQIPHNUzc0NRHrWPg2mNs9CmO1Y3-WA")
+var fyersdata= new FyersSocket("Y4PJ649WNP-100:eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJhcGkuZnllcnMuaW4iLCJpYXQiOjE2OTUwMTMwMDQsImV4cCI6MTY5NTA4MzQ0NCwibmJmIjoxNjk1MDEzMDA0LCJhdWQiOlsieDowIiwieDoxIiwieDoyIiwiZDoxIiwiZDoyIiwieDoxIiwieDowIl0sInN1YiI6ImFjY2Vzc190b2tlbiIsImF0X2hhc2giOiJnQUFBQUFCbEI5aU12Z0w0eUllZHhnSHNkX1NXR1c0czhER0lkeXBiLThtUjNIaEVxY0VEeTN6eVJKLUV0UERqWk5zRnpSMUNEbUVjOFZFTFBmOFVVWnpZOGRsQ1JMQ0JONThTSVNLQkVsOFdGTFZKRUU3WGFzOD0iLCJkaXNwbGF5X25hbWUiOiJLSFVTSFdJTkRFUiIsIm9tcyI6IksxIiwiaHNtX2tleSI6IjY2ZTBiYzM3MmQ4ZWVkMmNkNDdhNmJjNmJmYjY4MzY4MWRlN2Q5ZTg1MGU2YTlhYWI5NjgzMTkyIiwiZnlfaWQiOiJYSzAzNDgxIiwiYXBwVHlwZSI6MTAwLCJwb2FfZmxhZyI6Ik4ifQ.ZOyXv1MP-Gmba9i7aOaMNxSja1UXKFdl9DbhlgVf7gw")
 
 function onmsg(message){
           s = message.symbol
           l = message.ltp
     app.get('/api', (req, res) => {
         res.json({symbol:s, ltp:l});
+       
       });
       
-    // console.log()
+      console.log(message);
     }   
 
 
